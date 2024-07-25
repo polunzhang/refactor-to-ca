58cbab04b0b20da41cc0ca87576fb177fde98c94
polun <polunzhang@gmail.com>
2024-07-25 14:48:21 +0800

    init

---
+++ src/test/java/com/polunzhang/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polunzhang.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
